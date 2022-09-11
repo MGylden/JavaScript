9b478ca51243e0620a228f29ac02cc2f8a5fcd2e
MGylden <mathias@gylden.net>
2022-09-11 21:53:43 +0200

    updated app.js
    multiple functions added

--- assignment4/app.js
+++ assignment4/app.js
@@ -10,10 +10,10 @@ const salaryTransferToBankElement = document.getElementById("transferSalaryToBan
 const loanBalanceElement = document.getElementById("loanBalance");
 
 let computers = [];
-let loan = 0;
-let bankBalance = 500;
-let salaryBalance = 500;
-loanBalanceElement.innerHTML = parseInt(loan);
+let loanBalance = 0;
+let loanExist = false;
+let bankBalance = 000;
+let salaryBalance = 000;
 bBalanceElement.innerHTML = parseInt(bankBalance);
 salaryBalanceElement.innerHTML = parseInt(salaryBalance);
 
@@ -46,19 +46,76 @@ const handleBankBalance = () => {
 
 const handleGetLoan = () => {
     const loanPrompt = prompt("Please enter the amount you wish to loan");
-    let loanBalance = parseInt(bankBalance*2);
-    let loans = 0;
-    if (loans>1) {
-        if (loanBalance>bankBalance) {
-            loan = parseInt(loanUserInput+loan);
-            loans++;
+    let loanAmount = loanPrompt;
+    let loanMax = parseInt(bankBalance)*2;
+    if (loanExist===false){ 
+        if (loanPrompt<=loanMax){ 
+            loanBalance = parseInt(loanAmount) + loanBalance;
+            loanExist = true;
+            loanBalanceElement.innerText = parseInt(loanBalance);
+        }
     }
-    }
-    
-} 
+}
 
-const handleSalaryBalance = () => {
-    salaryBalance;
+const handleWorkButton = () => {
+    salaryBalance = parseInt(salaryBalance)+100;
+    salaryBalanceElement.innerText = parseInt(salaryBalance);
+    if (loanBalance<=0) {
+        loanExist = false;
+    }
+}
+
+
+const transfSalToBank = () => {
+    if(loanExist==true){
+        let tmpbankBalance = (90/100)*parseInt(salaryBalance);
+        bankBalance = parseInt(tmpbankBalance)+bankBalance;
+        let tmpLoanBalance = parseInt(salaryBalance) * 0.1;
+        loanBalance = parseInt(loanBalance) - parseInt(tmpLoanBalance);
+        salaryBalance = 0;
+        bBalanceElement.innerText = parseInt(bankBalance);
+        salaryBalanceElement.innerText = parseInt(salaryBalance);
+        loanBalanceElement.innerText = parseInt(loanBalance);
+        if (loanBalance <= 0) {
+            loanExist = false
+        }
+    }else{
+        bankBalance=parseInt(salaryBalance)+bankBalance;
+        salaryBalance = 0;
+        bBalanceElement.innerText = parseInt(bankBalance);
+        salaryBalanceElement.innerText = parseInt(salaryBalanceElement);
+        if (loanBalance<=0) {
+            loanExist = false;
+        }
+    }
+}
+
+const handlePayLoan = () => {
+    //Chore: Hide button
+    if (loanExist==true) {
+        //Chore: show button
+        if (loanBalance>=salaryBalance) {
+            loanBalance = parseInt(loanBalance) - salaryBalance;
+            salaryBalance = 0;
+            
+            loanBalanceElement.innerText = parseInt(loanBalance);
+            salaryBalanceElement.innerText = parseInt(salaryBalance);
+            loanExist = false;
+        }else{
+            loanBalance = parseInt(loanBalance) - salaryBalance;
+            salaryBalance = 0;
+            let tempLoanBalance = Math.abs(loanBalance);
+
+            if(loanBalance<0){
+                bankBalance = parseInt(tempLoanBalance)+bankBalance;
+                loanBalance = 0;
+                loanBalanceElement.innerText = parseInt(loanBalance);
+                salaryBalanceElement.innerText = parseInt(salaryBalance);
+                bBalanceElement.innerText = parseInt(bankBalance);
+                loanExist=false;
+        }
+        }
+    }
 }
 
 const handlePay = () => {
@@ -68,6 +125,8 @@ const handlePay = () => {
         if (change>=0) {
             alert('Congratulations on your new '+ selectedPc.title + 
             '\nTotal change: ' + change);
+            bankBalance = parseInt(bankBalance) - totalPay;
+            bBalanceElement.innerText = parseInt(bankBalance);
         }else{
             alert('No pain, no gain, go work more and come back when you are rich')
         }
@@ -75,6 +134,8 @@ const handlePay = () => {
 
 computersElement.addEventListener("change", handleComputerChange);
 bBalanceElement.addEventListener("change", handleBankBalance);
-salaryBalanceElement.addEventListener("change", handleSalaryBalance);
+salaryBalanceElement.addEventListener("change",);
 payElement.addEventListener("click", handlePay);
 loanBalanceElement.addEventListener("change", handleGetLoan);
+workElement.addEventListener("click", handleWorkButton)
+salaryTransferToBankElement.addEventListener("click", transfSalToBank);
